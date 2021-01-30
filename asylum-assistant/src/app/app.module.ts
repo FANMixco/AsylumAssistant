900840a65baf40171827259ea83ac33450029db5
Federico Navarrete <fanmixco@gmail.com>
2021-01-30 13:13:10 +0100

    Home
    Added correct bootstrap

--- asylum-assistant/src/app/app.module.ts
+++ asylum-assistant/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CountriesSelectComponent } from './home/countries-select/countries-select.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesSelectComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ asylum-assistant/src/app/home/countries-select/countries-select.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-select',
+  templateUrl: './countries-select.component.html',
+  styleUrls: ['./countries-select.component.css']
+})
+export class CountriesSelectComponent implements OnInit {
+
+  selectedCountry: number = -1;
+
+  countriesList: Array<any> = [
+    {
+      "id": 1,
+      "name": "المغرب/Maroc",
+      "flag": "morocco.png"
+    },
+    {
+      "id": 2,
+      "name": "تونس/Tunisie",
+      "flag": "tunisia.png"
+    },
+    {
+      "id": 3,
+      "name": "ليبيا/Libye",
+      "flag": "libya.png"
+    },
+    {
+      "id": 4,
+      "name": "سوريا/Syria",
+      "flag": "syria.png"
+    },
+    {
+      "id": 5,
+      "name": "عربي صحراوي/RASD",
+      "flag": "sahrawi.png"
+    },
+    {
+      "id": 6,
+      "name": "Nigeria",
+      "flag": "nigeria.png"
+    },
+    {
+      "id": 7,
+      "name": "Mali",
+      "flag": "mali.png"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.selectedCountry = -1;
+    //this.selectedCountry = this.countriesList.filter(a => a.defaultSelected)[0].value;
+  }
+
+  public changeValue() {
+    this.selectedCountry = this.countriesList.filter(a => a.defaultSelected)[0].value;
+  }
+
+}
