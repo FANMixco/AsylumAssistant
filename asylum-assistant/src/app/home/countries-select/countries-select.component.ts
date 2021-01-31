a59f49066caee4f9503ed81d79d907cb7fcd7582
Federico Navarrete <fanmixco@gmail.com>
2021-01-31 21:18:21 +0100

    Added basic UI for countries

--- asylum-assistant/src/app/home/countries-select/countries-select.component.ts
+++ asylum-assistant/src/app/home/countries-select/countries-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+const countries = require('../../../assets/json/countries.json');
 
 @Component({
   selector: 'app-countries-select',
@@ -9,43 +10,7 @@ export class CountriesSelectComponent implements OnInit {
 
   selectedCountry: number = -1;
 
-  countriesList: Array<any> = [
-    {
-      "id": 1,
-      "name": "المغرب/Maroc",
-      "flag": "./assets/images/morocco.png"
-    },
-    {
-      "id": 2,
-      "name": "تونس/Tunisie",
-      "flag": "./assets/images/tunisia.png"
-    },
-    {
-      "id": 3,
-      "name": "ليبيا/Libye",
-      "flag": "./assets/images/libya.png"
-    },
-    {
-      "id": 4,
-      "name": "سوريا/Syria",
-      "flag": "./assets/images/syria.png"
-    },
-    {
-      "id": 5,
-      "name": "عربي صحراوي/RASD",
-      "flag": "./assets/images/sahrawi.png"
-    },
-    {
-      "id": 6,
-      "name": "Nigeria",
-      "flag": "./assets/images/nigeria.png"
-    },
-    {
-      "id": 7,
-      "name": "Mali",
-      "flag": "./assets/images/mali.png"
-    }
-  ];
+  countriesList: Array<any> = countries;
 
   constructor() { }
 
