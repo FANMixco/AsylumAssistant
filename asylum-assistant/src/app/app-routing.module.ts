0af0c3b4af2d882a0249474d68d82a6030f4e4f3
Federico Navarrete <fanmixco@gmail.com>
2021-02-02 06:37:19 +0100

    Added routing

--- asylum-assistant/src/app/app-routing.module.ts
+++ asylum-assistant/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountriesSelectComponent } from './home/countries-select/countries-select.component';
+import { IntroComponent } from './intro/intro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'intro', component: IntroComponent },
+  { path: 'countries-select', component: CountriesSelectComponent },
+  { path: '',   redirectTo: '/countries-select', pathMatch: 'full' },
+  { path: '**', component: CountriesSelectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
