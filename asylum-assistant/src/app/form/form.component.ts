baba1bc4490da777165f1d52f97de83b31535bf8
Federico Navarrete <fanmixco@gmail.com>
2021-02-04 15:05:15 +0100

    Added basic form

--- asylum-assistant/src/app/form/form.component.ts
+++ asylum-assistant/src/app/form/form.component.ts
@@ -7,13 +7,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  currentTab = 0; // Current tab is set to be the first tab (0)
+
   constructor() {
     if (!sessionStorage.getItem('lng')) {
       window.location.href = `${document.location.origin}/home`;
     }
   }
 
-  ngOnInit(): void {
+  showTab(n:number) {
+    // This function will display the specified tab of the form ...
+    let x = document.getElementsByClassName("tab") as HTMLCollectionOf<HTMLElement>;
+    x[n].style.display = "block";
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+      document.getElementById("prevBtn").style.display = "none";
+    } else {
+      document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+      document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    // ... and run a function that displays the correct step indicator:
+    this.fixStepIndicator(n)
   }
+
+  nextPrev(n:number) {
+    // This function will figure out which tab to display
+    let x = document.getElementsByClassName("tab") as HTMLCollectionOf<HTMLElement>;
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !this.validateForm()) return false;
+    // Hide the current tab:
+    x[this.currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    this.currentTab = this.currentTab + n;
+    // if you have reached the end of the form... :
+    if (this.currentTab >= x.length) {
+      //...the form gets submitted:
+      //document.getElementById("regForm").submit();
+      return false;
+    }
+    // Otherwise, display the correct tab:
+    this.showTab(this.currentTab);
+  }
+
+  validateForm() {
+    // This function deals with validation of the form fields
+    let x:any, y:any, i:any, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[this.currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+      // If a field is empty...
+      if (y[i].value == "") {
+        // add an "invalid" class to the field:
+        y[i].className += " invalid";
+        // and set the current valid status to false:
+        valid = false;
+      }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+      document.getElementsByClassName("step")[this.currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+  }
+
+  fixStepIndicator(n:number) {
+    // This function removes the "active" class of all steps...
+    let i:number, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+      x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+  }
+
+  ngOnInit(): void {
+    this.showTab(this.currentTab); // Display the current tab
+  }
+
+
 
 }
