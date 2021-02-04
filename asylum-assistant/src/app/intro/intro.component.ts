624d4fdef212d634415ab8502980363a5790e7fe
Federico Navarrete <fanmixco@gmail.com>
2021-02-04 20:34:04 +0100

    Configured translate plugin

--- asylum-assistant/src/app/intro/intro.component.ts
+++ asylum-assistant/src/app/intro/intro.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { TranslateService } from '@ngx-translate/core';
+import { Globals } from '../globals';
 
 @Component({
   selector: 'app-intro',
@@ -9,9 +12,25 @@ import { ActivatedRoute } from "@angular/router";
 export class IntroComponent implements OnInit {
 
   isDivVisible = false;
+  lng = 'en';
+  path = '';
 
-  constructor(private route: ActivatedRoute) {
-    sessionStorage.setItem('lng', this.route.snapshot.paramMap.get("lng"));
+  readonly defaultVideo = `${window.location.origin}/assets/videos/en/intro.mp4`;
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient, private translateService: TranslateService, private globals: Globals) {
+    this.lng = this.route.snapshot.paramMap.get("lng");
+    sessionStorage.setItem('lng', this.lng);
+    this.path = this.getVideoPath();
+    this.setLng();
+  }
+
+  setLng():void {
+    let tmpLng = 'en';
+
+    if (this.globals.availableLng.includes(this.lng))
+      tmpLng = this.lng;
+
+    this.translateService.setDefaultLang(tmpLng);
   }
 
   ngOnInit(): void {
@@ -19,5 +38,18 @@ export class IntroComponent implements OnInit {
 
   introEnded(): void {
     this.isDivVisible = true;
+  }
+
+  getVideoPath(): string {
+    let tmpPath = `${window.location.origin}/assets/videos/${this.lng}/intro.mp4`;
+    this.httpClient.get(tmpPath).subscribe(() => {
+      return tmpPath;
+    }, (err) => {
+      // HANDLE file not found
+      if (err.status === 404) {
+        return this.defaultVideo;
+      }
+    });
+    return this.defaultVideo;
   }
 }
