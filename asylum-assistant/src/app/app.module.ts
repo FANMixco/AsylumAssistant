7692b26e511ae63246cf730d18af0bdae9dbc3d7
Federico Navarrete <fanmixco@gmail.com>
2021-02-04 19:56:55 +0100

    Added multi-language support

--- asylum-assistant/src/app/app.module.ts
+++ asylum-assistant/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,14 @@ import { IntroComponent } from './intro/intro.component';
 import { OptionsComponent } from './options/options.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Globals } from './globals';
+
+export function translateHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+  //return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -25,9 +34,33 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [ Globals ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private translateService: TranslateService, private globals: Globals) {
+    this.setLng();
+  }
+
+  setLng():void {
+    let tmpLng = 'en';
+
+    let currentLng = window.navigator.language.substring(0, 2);
+
+    if (this.globals.availableLng.includes(currentLng))
+      tmpLng = currentLng;
+
+    this.translateService.setDefaultLang(tmpLng);
+  }
+}
